23817ceae707871bdf4da81e3a4e6648edefeb49
Ramon Diaz <50044994+ramndiaz@users.noreply.github.com>
2022-07-18 17:55:48 -0400

    first commit

---
+++ src/app.py
@@ -0,0 +1,72 @@
+from flask import Flask
+from dotenv import load_dotenv
+from flask import request, jsonify, Response
+from flask_pymongo import PyMongo, pymongo
+from pymongo.mongo_client import MongoClient      #prueba
+from pymongo.server_api import ServerApi    #prueba
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from flask_cors import CORS, cross_origin
+from datetime import timedelta
+import os
+
+
+app = Flask(__name__)
+
+#dotenv
+load_dotenv()
+
+# Setup Flask-JWT-Extended
+ACCESS_EXPIRES = timedelta(hours=4)
+app.config["JWT_SECRET_KEY"] = os.getenv('CONFIG_SECRET_KEY')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
+jwt = JWTManager(app)
+
+# Cross Origin
+CORS(app)
+
+# Setup Database
+#app.config['MONGO_URI'] = os.getenv('CONFIG_SECRET_DATABASE')
+#mongo = PyMongo(app)
+
+
+#client = pymongo.MongoClient("mongodb+srv://ramn:Samsung01@pomodorodb.htif2.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+client = pymongo.MongoClient(os.getenv('CONFIG_SECRET_DATABASE'), server_api=ServerApi('1'))
+mongo = client.test
+
+
+
+# Routes
+@cross_origin
+@app.route('/')
+@app.route('/home')
+@jwt_required(optional=True)
+def home():
+    current_identity = get_jwt_identity()
+    if current_identity:
+        response = jsonify(logged_in_as=current_identity)
+        response.status_code=200
+        return response
+    else:
+        response = jsonify(logged_in_as="anonymous user")
+        response.status_code=200
+        return response
+
+@cross_origin
+@app.route("/dashboard/")
+@jwt_required(optional=False)
+def dashboard():
+    current_user = get_jwt_identity()
+    response = jsonify(logged_in_as=current_user)
+    response.status_code=200
+    return response
+
+
+from user import routes
+from auth import routes
+#from Oauth import routes
+from tasks import routes
+from tracker import routes
+
+
+if __name__=="__main__":
+	app.run(port=5000)
---
+++ src/user/routes.py
@@ -0,0 +1,92 @@
+from flask import Flask, jsonify, request, session, redirect, Response, url_for
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS, cross_origin
+from bson import json_util
+from __main__ import app
+from app import mongo, jwt
+
+@cross_origin
+@app.route('/getuser/', methods=['GET'])
+@jwt_required(optional=False)
+def get_user():
+    username = get_jwt_identity()
+    if username:
+        user = mongo.db.users.find_one({'username': username})
+        response = json_util.dumps(user)
+        return Response(response, mimetype='application/json')
+    else:
+        response = jsonify({"error": "user not found"})
+        response.status_code=401
+        return response
+
+@cross_origin
+@app.route('/createuser', methods=['POST'])
+@jwt_required(optional=True)
+def create_user():
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+    if email and username and password :
+        #check minimum password length
+        if len(password) < 7:
+            response = jsonify({ "error":"Password must be at least 7 characters." })
+            response.status_code=403
+            return response
+        #hash the password
+        hashed_password = generate_password_hash(password)
+        #if email exist
+#
+        if mongo.db.users.find_one({ "email": email }):
+            response = jsonify({ "error":"Email address already in use" })
+            response.status_code=403
+            return response
+        #if username exist
+        if mongo.db.users.find_one({ "username": username }):
+            response = jsonify({ "error":"username already in use" })
+            response.status_code=403
+            return response
+#
+        try: mongo.db.users.insert_one({
+                'email' : email,
+                'username' : username,
+                'password' : hashed_password
+                })
+        except:
+            response = jsonify({"msg": "No hosts found"})
+            response.status_code=401
+            return response
+        response = jsonify({"msg": "user created successfully"})
+        response.status_code=200
+        return response
+    else:
+        response = jsonify({"error": "data could not be captured"})
+        response.status_code=401
+        return response
+
+@cross_origin
+@app.route('/updatepassword', methods=['PUT'])
+@jwt_required(optional=False)
+def update_password():
+    username = get_jwt_identity()
+    password = request.json['password']
+    if username and password :
+         #check minimum password length
+        if len(password) < 7:
+            response = jsonify({ "error":"Password must be at least 7 characters." })
+            response.status_code=403
+            return response
+        #hash the password
+        hashed_password = generate_password_hash(password)
+        #search for usrname
+        if mongo.db.users.find_one({ "username": username }):
+            mongo.db.users.update_one({'username': username}, {'$set':{
+                'password': hashed_password,
+            }})
+            response = jsonify({'messsage': ' user: ' + username + ' has updated the password'})
+            response.status_code=200
+            return response
+    else:
+        response = jsonify({"error": "data could not be captured"})
+        response.status_code=401
+        return response
